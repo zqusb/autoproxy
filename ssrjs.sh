1b1a7855cc2fa4ff8a778a4223ab2048fa873d90
zqusb <58019434+zqusb@users.noreply.github.com>
2019-11-23 13:23:02 +0800

    Update ssrjs.sh

--- ssrjs.sh
+++ ssrjs.sh
@@ -2,14 +2,6 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-#=================================================
-#	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Description: Install the ShadowsocksR server
-#	Version: 2.0.38
-#	Author: Toyo
-#	Blog: https://doub.io/ss-jc42/
-#=================================================
-
 sh_ver="2.0.38"
 filepath=$(cd "$(dirname "$0")"; pwd)
 file=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
